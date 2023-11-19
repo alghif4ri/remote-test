54c7faa22ff2b907881762bbe72e2605c5c64f48
LuckyNoobz <r.abudzar@gmail.com>
2023-11-19 15:27:46 +0700

    update index

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore commodi unde nihil iste. Error, veniam.</p>
     <ul>
         <li>satu</li>
-        <li>dua</li>
     </ul>
 </body>
 </html>
