e6b05f88b79c81e1ce17a73c5ff2626b0fd1b1ec
LuckyNoobz <r.abudzar@gmail.com>
2023-11-19 15:16:02 +0700

    update index.php add list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Test Git Remote</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore commodi unde nihil iste. Error, veniam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
